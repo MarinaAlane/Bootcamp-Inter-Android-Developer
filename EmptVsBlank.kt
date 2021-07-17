81ef8333363777ba3a158bad2b7a179cf430f450
Marina Alane <marina.alane@outlook.com>
2021-07-17 17:25:29 -0300

    manipulando strings

---
+++ EmptVsBlank.kt
@@ -0,0 +1,14 @@
+// Métodos de comparação 
+
+// STring vazia = tamanho é 0
+
+// string que só tem espaços em branco não está vazia, mas está em branco 
+
+val s = ""
+println(s.isEmpty())
+println(s.isBlank())
+
+
+val m = "         "
+println(m.isBlank())
+println(m.isEmpty())
---
+++ manipulandoString.kt
@@ -0,0 +1,30 @@
+// Pode ser concatenada com plus/+ => um método não seguro
+// tbm é tratada como um array de char
+
+// Indexação - First(), last(), length() e index[0]
+
+// Concatenação parecida com o template string do JS
+
+// // função principal
+fun main() {
+  val name = "Marina"
+    val lastName = "Alane"
+  println(name + lastName)
+// A melhor forma nesse cenário
+println("${name} ${lastName}")
+// RANGE = ..
+println("Oi, ${name}")
+}
+
+// Método de formatação
+
+// capitalize() = > primeira letra maiúscula
+// decapitalize => primeira letra minúsculo
+
+// toUpperCase e toLowerCase => como no JS
+
+
+// trim() -> retira espaco vazio da string 
+// trimEnd(), trimStart()
+
+// SUbstituir caracter replace(x,y) => Como no JS
