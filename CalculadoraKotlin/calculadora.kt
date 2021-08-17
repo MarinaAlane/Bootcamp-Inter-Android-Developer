3ff813c8ddedc9094f2dd87aa3acc9d57237a50d
Marina Alane <marina.alane@outlook.com>
2021-08-17 10:31:43 -0300

    calculadora

---
+++ CalculadoraKotlin/calculadora.kt
@@ -0,0 +1,17 @@
+// função principal
+fun main(args: Array<String>) {
+    val x = 10
+    val y = 2
+    
+   val resultado = soma(x,y)
+   
+   println("O resultado da soma é $resultado")
+}
+
+fun soma(x: Int, y:Int)= x+y
+
+fun multiplica(x: Int, y:Int)= x*y
+
+fun subtrai(x: Int, y: Int)= x-y
+
+fun divide(x: Int, y: Int)= x/y
